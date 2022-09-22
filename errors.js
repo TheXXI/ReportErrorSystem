400729eda6614f3c4bb6b4f023cc45789ad449ee
TheXXI <58274104+TheXXI@users.noreply.github.com>
2022-09-22 16:29:18 +0300

    Update errors.js

--- errors.js
+++ errors.js
@@ -8,27 +8,19 @@ document.getElementById('showAllButton'),
 
 const table = document.getElementById('errorsTable')
 const errorsTableRows = table.getElementsByTagName('tr')
-//console.log(errorsTableRows)
-
-console.log(errorsTableRows)
 
 buttons.forEach((button, index) => {
     button.addEventListener("click", () => showErrors(index))
 })
 
-
 function showErrors(index) {
     for (let i = 1; i < errorsTableRows.length; i++) {
         errorsTableRows[i].classList.remove('hide')
     }
-
-    console.log("click")
-
     buttons.forEach((button) => {
         button.classList.remove('btn-primary')
         button.classList.add('btn-outline-primary')
     })
-
     buttons[index].classList.add('btn-primary')
     buttons[index].classList.remove('btn-outline-primary')
 
@@ -48,25 +40,6 @@ function showErrors(index) {
         }
         if (index == 3 && status != "Закрытая") {
             errorsTableRows[i].classList.add('hide')
-        }
-        
+        }   
     }
-
 }
-
-
-
-//buttons[2].classList.add('btn-primary')
-//buttons[2].classList.remove('btn-outline-primary')
-//const startGameBtn = document.getElementById('startGameBtn')
-//const loadingText = document.getElementById('loadingText')
-
-//dev
-//startGameClick()
-
-//startGameBtn.addEventListener("click", startGameClick);
-
-//function startGameClick() {
-    //startGameBtn.classList.add('hide')
-    //loadingText.classList.remove('hide')
-    //sudoku = sudokuGenerator(new Sudoku())
